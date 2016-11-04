b18bccd2293745915ad26f21c684c477e5020e70
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-04 13:25:36 -0700

    Add to and restructure the frontend directory.

---
+++ habitualizer_frontend/js/app/app.js
@@ -0,0 +1,11 @@
+angular
+  .module('app', ['Devise'])
+  .controller('myCtrl', function(Auth) {
+        Auth.currentUser().then(function(user) {
+            // User was logged in, or Devise returned
+            // previously authenticated session.
+            console.log(user); // => {id: 1, ect: '...'}
+        }, function(error) {
+            // unauthenticated error
+        });
+      });
