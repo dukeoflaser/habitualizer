5f6bb86844315f965a72469eb874b0e0014f802b
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-04 18:08:18 -0700

    Add login state with temporary test template. Display it in the index.

--- habitualizer_frontend/js/app/app.js
+++ habitualizer_frontend/js/app/app.js
@@ -2,4 +2,12 @@ angular
   .module('app', ['Devise', 'ui.router'])
   .config(function(AuthProvider){
     AuthProvider.baseUrl('http://localhost:3000');
-  });
+  })
+  .config(function ($stateProvider) {
+        $stateProvider
+            .state('login', {
+                url: '/users/sign_up',
+                template: '<div>This is the login page.</div>',
+                controller: 'LoginController'
+            });
+    });
