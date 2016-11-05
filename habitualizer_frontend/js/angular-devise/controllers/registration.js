84945a1274bc7ae680d3f1b2163d0a6013a3cfbb
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-05 04:51:29 -0700

    Add functionality to registration form.

--- habitualizer_frontend/js/angular-devise/controllers/registration.js
+++ habitualizer_frontend/js/angular-devise/controllers/registration.js
@@ -1,19 +1,19 @@
 angular
   .module('app')
   .controller('RegistrationController', function(Auth, $scope) {
-    var credentials = {
-        email: 'user@domain.com',
-        password: 'password1',
-        password_confirmation: 'password1'
-    };
 
-    this.controller = 'RegistrationController';
+    this.submit = function(user){
+      var credentials = user;
+      console.log(user);
 
-    Auth.register(credentials).then(function(registeredUser) {
-        console.log(registeredUser); // => {id: 1, ect: '...'}
-    }, function(error) {
-        // Registration failed...
-    });
+      Auth.register(credentials).then(function(registeredUser) {
+          console.log(registeredUser); // => {id: 1, ect: '...'}
+      }, function(error) {
+          // Registration failed...
+      });
+    }
+
+
 
     $scope.$on('devise:new-registration', function(event, user) {
         // ...
