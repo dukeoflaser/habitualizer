8ab41209a002dc4375345bc925a5ce7ada834c3d
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-04 17:20:01 -0700

    Move test contoller over to /angular-devise/controllers and include it as LoginController.

---
+++ habitualizer_frontend/js/angular-devise/controllers/login.js
@@ -0,0 +1,25 @@
+angular
+  .module('app')
+  .controller('LoginController', function(Auth, $scope) {
+        var credentials = {
+            email: 'palu@email.com',
+            password: 'password'
+        };
+
+
+        this.controller = 'LoginController';
+
+        Auth.login(credentials).then(function(user) {
+            console.log(user); // => {id: 1, ect: '...'}
+        }, function(error) {
+            // Authentication failed...
+        });
+
+        $scope.$on('devise:login', function(event, currentUser) {
+            // after a login, a hard refresh, a new tab
+        });
+
+        $scope.$on('devise:new-session', function(event, currentUser) {
+            // user logged in by Auth.login({...})
+        });
+    });
--- habitualizer_frontend/js/app/app.js
+++ habitualizer_frontend/js/app/app.js
@@ -3,29 +3,3 @@ angular
   .config(function(AuthProvider){
     AuthProvider.baseUrl('http://localhost:3000');
   });
-
-angular
-  .module('app')
-  .controller('myCtrl', function(Auth, $scope) {
-        var credentials = {
-            email: 'palu@email.com',
-            password: 'password'
-        };
-
-
-        $scope.test = 'Test';
-
-        Auth.login(credentials).then(function(user) {
-            console.log(user); // => {id: 1, ect: '...'}
-        }, function(error) {
-            // Authentication failed...
-        });
-
-        $scope.$on('devise:login', function(event, currentUser) {
-            // after a login, a hard refresh, a new tab
-        });
-
-        $scope.$on('devise:new-session', function(event, currentUser) {
-            // user logged in by Auth.login({...})
-        });
-    });
