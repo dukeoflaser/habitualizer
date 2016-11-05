26848c9bee0473c1c32f00545482c150ec354849
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-05 16:38:27 -0700

    Nest habit and experiment routes under user

--- habitualizer_api/config/routes.rb
+++ habitualizer_api/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :user do
+    resources :habits, only: [:index]
+    resources :experiments, only: [:index]
+  end
+
   resources :activities, only: [:index, :show, :create, :update, :destroy]
   resources :cues, only: [:index, :show, :create, :update, :destroy]
-  resources :experiments, only: [:index, :show, :create, :update, :destroy]
-  resources :habits, only: [:index, :show, :create, :update, :destroy]
+  resources :experiments, only: [:show, :create, :update, :destroy]
+  resources :habits, only: [:show, :create, :update, :destroy]
   resources :rewards, only: [:index, :show, :create, :update, :destroy]
 
 end
