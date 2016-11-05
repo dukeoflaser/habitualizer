945ec437c370a97a946d70deb24eb46a0b993b53
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-05 13:56:30 -0700

    Add Experiment model. Juggle associations arounds. Currently User->Habit->Reward/Experiments->Reward(aka substitute).

--- habitualizer_api/app/models/habit.rb
+++ habitualizer_api/app/models/habit.rb
@@ -1,5 +1,7 @@
 class Habit < ApplicationRecord
   belongs_to :user
   has_many :cues
+  has_many :experiments
   has_one :reward
+  has_many :substitutes, through: :experiments
 end
--- habitualizer_api/app/models/reward.rb
+++ habitualizer_api/app/models/reward.rb
@@ -1,3 +1,4 @@
 class Reward < ApplicationRecord
   belongs_to :habit
+  belongs_to :experiment
 end
