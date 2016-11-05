6d348a307e9e92894678de4b1bde91ac5804d6dd
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-05 04:16:32 -0700

    Add functionality to login form inputs. Current state: If a user exists in the database, a matching email and password will that user object.

--- habitualizer_frontend/js/angular-devise/controllers/login.js
+++ habitualizer_frontend/js/angular-devise/controllers/login.js
@@ -1,25 +1,24 @@
 angular
   .module('app')
   .controller('LoginController', function(Auth, $scope) {
-        var credentials = {
-            email: 'palu@email.com',
-            password: 'password'
-        };
 
+    this.submit = function(user) {
+      var credentials = user;
+      console.log(credentials);
 
-        this.controller = 'LoginController';
+      Auth.login(credentials).then(function(user) {
+          console.log(user); // => {id: 1, ect: '...'}
+      }, function(error) {
+          // Authentication failed...
+      });
 
-        Auth.login(credentials).then(function(user) {
-            console.log(user); // => {id: 1, ect: '...'}
-        }, function(error) {
-            // Authentication failed...
-        });
+    };
 
-        $scope.$on('devise:login', function(event, currentUser) {
-            // after a login, a hard refresh, a new tab
-        });
-
-        $scope.$on('devise:new-session', function(event, currentUser) {
-            // user logged in by Auth.login({...})
-        });
+    $scope.$on('devise:login', function(event, currentUser) {
+        // after a login, a hard refresh, a new tab
     });
+
+    $scope.$on('devise:new-session', function(event, currentUser) {
+        // user logged in by Auth.login({...})
+    });
+  });
