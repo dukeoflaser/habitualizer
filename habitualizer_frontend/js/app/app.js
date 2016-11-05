a712ed1b3a63ccd025013f2ef3957aba0b8f8105
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-05 10:06:52 -0700

    Add 'root' state. This is the 'welcome' page that directs the user to either login or register. Create welcome view.  Move login/register links from index.html to welcome view.

--- habitualizer_frontend/js/app/app.js
+++ habitualizer_frontend/js/app/app.js
@@ -4,28 +4,32 @@ angular
     AuthProvider.baseUrl('http://localhost:3000');
   })
   .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: 'js/app/views/home.html',
-                controller: 'HomeController',
-                controllerAs: 'user',
-                resolve: {
-                  user: ['Auth', function(Auth){
-                    return Auth.currentUser();
-                  }]
-                }
-            })
-            .state('login', {
-                url: '/users/sign_in',
-                templateUrl: 'js/angular-devise/views/login.html',
-                controller: 'LoginController'
-            })
-            .state('register', {
-                url: '/users/sign_up',
-                templateUrl: 'js/angular-devise/views/registration.html',
-                controller: 'RegistrationController'
-            });
+    $stateProvider
+      .state('root', {
+        url: '/',
+        templateUrl: 'js/app/views/welcome.html'
+      })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'js/app/views/home.html',
+        controller: 'HomeController',
+        controllerAs: 'user',
+        resolve: {
+          user: ['Auth', function(Auth){
+            return Auth.currentUser();
+          }]
+        }
+      })
+      .state('login', {
+        url: '/users/sign_in',
+        templateUrl: 'js/angular-devise/views/login.html',
+        controller: 'LoginController'
+      })
+      .state('register', {
+        url: '/users/sign_up',
+        templateUrl: 'js/angular-devise/views/registration.html',
+        controller: 'RegistrationController'
+      });
 
-            $urlRouterProvider.otherwise('/');
+      $urlRouterProvider.otherwise('/');
     });
