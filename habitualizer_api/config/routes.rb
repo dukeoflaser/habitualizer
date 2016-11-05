bb6e71e47161a0bb7d913bf89def381f1d94ec4b
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-05 16:00:44 -0700

    Add index, show, create, update, and destroy routes for each controller/model

--- habitualizer_api/config/routes.rb
+++ habitualizer_api/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :activities, only: [:index, :show, :create, :update, :destroy]
+  resources :cues, only: [:index, :show, :create, :update, :destroy]
+  resources :experiments, only: [:index, :show, :create, :update, :destroy]
+  resources :habits, only: [:index, :show, :create, :update, :destroy]
+  resources :rewards, only: [:index, :show, :create, :update, :destroy]
+
 end
