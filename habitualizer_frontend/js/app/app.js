5c964b21e458e442d46f00874fa21dfdbdec5e8b
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-06 04:47:36 -0800

    Add habits and experiments templates. Link to each from home template currently.

--- habitualizer_frontend/js/app/app.js
+++ habitualizer_frontend/js/app/app.js
@@ -31,62 +31,64 @@ angular
         }
       })
       .state('complete', {
-        url: '/complete'
+        url: '/complete',
+        templateUrl: 'js/app/views/habits/complete.html'
       })
       .state('experiments', {
-        url: '/experiments'
-      })
-      .state('habit', {
-        url: '/habit/:id'
-      })
-      .state('experiment', {
-        url: '/experiment/:id'
-      })
-
-
-      
-      .state('new', {
-        abstract: true,
-        url: '/new',
-        template: '<div ui-view></div>'
-      })
-      .state('new.habit', {
-        url: '/habit'
-      })
-      .state('new.cue', {
-        url: '/cue'
-      })
-      .state('new.reward', {
-        url: '/reward'
-      })
-      .state('new.experiment', {
-        url: '/experiment'
-      })
-      .state('new.activity', {
-        url: '/activity'
-      })
-
-
-      .state('edit', {
-        abstract: true,
-        url: '/edit',
-        template: '<div ui-view></div>'
-      })
-      .state('edit.habit', {
-        url: '/habit'
-      })
-      .state('edit.cue', {
-        url: '/cue'
-      })
-      .state('edit.reward', {
-        url: '/reward'
-      })
-      .state('edit.experiment', {
-        url: '/experiment'
-      })
-      .state('edit.activity', {
-        url: '/activity'
+        url: '/experiments',
+        templateUrl: 'js/app/views/experiments/experiments.html'
       });
+      // .state('habit', {
+      //   url: '/habit/:id'
+      // })
+      // .state('experiment', {
+      //   url: '/experiment/:id'
+      // })
+      //
+      //
+      //
+      // .state('new', {
+      //   abstract: true,
+      //   url: '/new',
+      //   template: '<div ui-view></div>'
+      // })
+      // .state('new.habit', {
+      //   url: '/habit'
+      // })
+      // .state('new.cue', {
+      //   url: '/cue'
+      // })
+      // .state('new.reward', {
+      //   url: '/reward'
+      // })
+      // .state('new.experiment', {
+      //   url: '/experiment'
+      // })
+      // .state('new.activity', {
+      //   url: '/activity'
+      // })
+      //
+      //
+      // .state('edit', {
+      //   abstract: true,
+      //   url: '/edit',
+      //   template: '<div ui-view></div>'
+      // })
+      // .state('edit.habit', {
+      //   url: '/habit'
+      // })
+      // .state('edit.cue', {
+      //   url: '/cue'
+      // })
+      // .state('edit.reward', {
+      //   url: '/reward'
+      // })
+      // .state('edit.experiment', {
+      //   url: '/experiment'
+      // })
+      // .state('edit.activity', {
+      //   url: '/activity'
+      // });
 
       $urlRouterProvider.otherwise('/');
     });
