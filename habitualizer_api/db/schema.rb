217144129937ea062914f6878b29ac340d6bee2c
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-06 16:04:56 -0800

    Add complete column to habits model

--- habitualizer_api/db/schema.rb
+++ habitualizer_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105223343) do
+ActiveRecord::Schema.define(version: 20161107000234) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "description"
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 20161105223343) do
   create_table "habits", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "complete",   default: false
   end
 
   create_table "rewards", force: :cascade do |t|
