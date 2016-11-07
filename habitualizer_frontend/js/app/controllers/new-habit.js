4a87a4efe02b2dca09a2f2cd50f3f36c4c423c86
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-06 18:55:57 -0800

    Get cues, rewards, and activities data and pass it to the new controller.

--- habitualizer_frontend/js/app/controllers/new-habit.js
+++ habitualizer_frontend/js/app/controllers/new-habit.js
@@ -1,6 +1,13 @@
 angular
   .module('app')
-  .controller('NewHabitController', function() {
+  .controller('NewHabitController', function(cues, rewards, activities) {
+
+    console.log('Cues');
+    console.log(cues);
+    console.log('Rewards');
+    console.log(rewards);
+    console.log('Activities');
+    console.log(activities);
 
 
   });
--- habitualizer_frontend/js/app/states/user-new-states.js
+++ habitualizer_frontend/js/app/states/user-new-states.js
@@ -1,7 +1,8 @@
 angular
   .module('app')
   .config(function ($stateProvider) {
-  $stateProvider
+    var baseUrl = 'http://localhost:3000'
+    $stateProvider
 
     .state('user.new', {
       abstract: true,
@@ -12,7 +13,27 @@ angular
     .state('user.new.habit', {
       url: '/habit',
       templateUrl: 'js/app/views/habits/new.html',
-      controller: 'NewHabitController'
+      controller: 'NewHabitController',
+      resolve: {
+        cues: function($http){
+          return $http({
+            method: 'GET',
+            url: baseUrl + '/cues'
+          });
+        },
+        rewards: function($http){
+          return $http({
+            method: 'GET',
+            url: baseUrl + '/rewards'
+          });
+        },
+        activities: function($http){
+          return $http({
+            method: 'GET',
+            url: baseUrl + '/activities'
+          });
+        }
+      }
     })
 
     .state('user.new.experiment', {
