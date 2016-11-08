4b165b8e9e3d964c658b5dc9e39a8db973829e90
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-07 21:12:24 -0800

    Create edit habit form. Prefill with data from corresponding habit. Change hardcoded value in show view, just to double check the dynamic capabilities and also use a habit with more data.

--- habitualizer_frontend/js/app/controllers/edit-habit.js
+++ habitualizer_frontend/js/app/controllers/edit-habit.js
@@ -1,9 +1,19 @@
 angular
   .module('app')
-  .controller('EditHabitController', function(habit) {
+  .controller('EditHabitController', function($scope, habit) {
+    var hbt = habit.data.habit;
 
+    $scope.habit = hbt;
+    $scope.habit.user_id = hbt.user_id;
+    $scope.habit.name = hbt.name;
+    $scope.habit.complete = hbt.complete;
+    $scope.habit.cue_attributes = hbt.cue;
+    $scope.habit.cue_attributes.name = hbt.cue.name;
+    $scope.habit.cue_attributes.nature = hbt.cue.nature;
+    $scope.habit.reward_attributes = hbt.reward;
+    $scope.habit.reward_attributes.craving = hbt.reward.craving;
 
-    console.log('The editting habit:');
+    console.log('The editing habit:');
     console.log(habit);
 
   });
