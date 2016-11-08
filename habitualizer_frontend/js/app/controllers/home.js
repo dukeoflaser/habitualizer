07ef7ac3abcb38237ab32bae4bf95656c4560847
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-07 22:31:50 -0800

    Iterate through habits on home page. Display as links to individual show pages.

--- habitualizer_frontend/js/app/controllers/home.js
+++ habitualizer_frontend/js/app/controllers/home.js
@@ -1,12 +1,14 @@
 angular
   .module('app')
   .controller('HomeController', function(user, userHabits) {
-    this.email = user.email;
+    var habits = userHabits.data.habits;
 
+    this.email = user.email;
+    this.habits = habits;
 
     console.log('The user:');
     console.log(user);
-    console.log('userHabits');
-    console.log(userHabits);
+    console.log('habits');
+    console.log(this.habits);
 
   });
