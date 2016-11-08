b0fa75bd06362bef02e7076e79ac56295cde14b6
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-07 22:04:33 -0800

    Load experiment data into edit experiment form. Issue with loading the corresponding habit_id. Comes back as undefined despite it having a value in the database.

--- habitualizer_frontend/js/app/controllers/edit-experiment.js
+++ habitualizer_frontend/js/app/controllers/edit-experiment.js
@@ -1,6 +1,15 @@
 angular
   .module('app')
-  .controller('EditExperimentController', function(experiment) {
+  .controller('EditExperimentController', function($scope, experiment) {
+
+    var exp = experiment.data.experiment;
+
+    $scope.experiment = exp;
+    $scope.experiment.habit_id = exp.habit_id;
+    $scope.experiment.successful = exp.successful;
+    $scope.experiment.substitute_attributes = exp.substitute;
+    $scope.experiment.substitute_attributes.craving = exp.substitute.craving;
+
 
 
     console.log('The editing experiment:');
