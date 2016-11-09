77ade84fb98cc6a667972fbff23a4ec2bfbdd5ee
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-08 21:31:58 -0800

    Add date created to Habit info.

--- habitualizer_api/app/serializers/habit_serializer.rb
+++ habitualizer_api/app/serializers/habit_serializer.rb
@@ -1,3 +1,3 @@
 class HabitSerializer < ActiveModel::Serializer
-  attributes :id, :name, :complete, :cue, :reward, :experiments
+  attributes :id, :name, :complete, :cue, :reward, :experiments, :created_at
 end
