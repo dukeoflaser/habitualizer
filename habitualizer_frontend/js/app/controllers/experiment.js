d31a547c52d8f6b1cc402fa2e1fa3dd9dfe30fbb
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-08 20:30:09 -0800

    Make complete experiment list a bit more legible.

--- habitualizer_frontend/js/app/controllers/experiment.js
+++ habitualizer_frontend/js/app/controllers/experiment.js
@@ -6,6 +6,10 @@ angular
 
     this.email = user.email;
     this.experiments = exps;
+    // this.experiments.substitute.name = exps.substitute.name;
+    console.log('experiments.substitute:');
+    console.log(exps);
+
 
 
     console.log('The user:');
