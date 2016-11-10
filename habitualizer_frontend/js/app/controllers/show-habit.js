d321790473b971e57efaf5690546729ff286d632
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-10 09:07:20 -0800

    Set up for experiment deletion along with parent habit.

--- habitualizer_frontend/js/app/controllers/show-habit.js
+++ habitualizer_frontend/js/app/controllers/show-habit.js
@@ -7,14 +7,14 @@ angular
     console.log('The habit:');
     console.log(habit);
 
-    $scope.delete = function(habitId){
-      var confirmDelete = confirm("Are you certain you want to delete this habit?")
+    $scope.delete = function(hbt){
+      var confirmDelete = confirm("Are you certain you want to delete this habit along with it's experiments?")
 
       if (confirmDelete) {
 
         var req = {
           method: 'DELETE',
-          url: 'http://localhost:3000/habits/' + habitId
+          url: 'http://localhost:3000/habits/' + hbt.id
         }
 
         $http(req)
@@ -22,7 +22,15 @@ angular
             $state.go('user.home');
           });
 
+        hbt.experiments.forEach(function(exp, index){
+
+          //need to refactor and use delete exp method here.
+
+        });
+
       }
+
+
     }
 
   });
