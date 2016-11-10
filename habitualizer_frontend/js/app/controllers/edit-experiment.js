f969a38641450741e19b0f552d98574050aa47fc
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-10 07:43:44 -0800

    Edit experiment controller takes resolved activity and scopes it. Activity description is available and maluable in view.

--- habitualizer_frontend/js/app/controllers/edit-experiment.js
+++ habitualizer_frontend/js/app/controllers/edit-experiment.js
@@ -1,17 +1,44 @@
 angular
   .module('app')
-  .controller('EditExperimentController', function($scope, $http, $state, experiment) {
+  .controller('EditExperimentController', function($scope, $http, $state, experiment, activity) {
 
     var exp = experiment.data.experiment;
+    var activity = activity.data.activity;
+
+
+
+
+    console.log('The Activity');
+    console.log(activity);
+
+    console.log('exp')
+    console.log(exp)
+
 
     $scope.experiment = exp;
     $scope.experiment.substitute_attributes = exp.substitute;
+    $scope.experiment.habit.activity_attributes = activity;
+    //double check to make sure habit/activity values are loaded.
 
 
-    console.log('The editing experiment:');
+    console.log('The Initial editing experiment:');
     console.log(experiment);
 
+    // if(exp.habit.activity_id){
+    //   var activity_req = {
+    //     method: 'GET',
+    //     url: 'http://localhost:3000/activity/' + exp.habit.activity_id
+    //   }
+    //
+    //   $http(activity_request)
+    //     .then(function(response){
+    //       exp.habit.activity_attributes = response.data
+    //     });
+    // }
+
+
     $scope.submit = function(exp){
+      console.log(exp);
       var experiment = { experiment: exp };
       var req = {
         method: 'PATCH',
@@ -26,6 +53,34 @@ angular
           //add catch method. Break into named functions
           $state.go('user.show.experiment', { id: response.data.experiment.id })
         });
+
+
+
+      if(exp.habit.activity_attributes){
+        //check to make sure that exp is successful
+
+        if(exp.successful == false){
+          exp.habit.activity_attributes.description = "";
+        }
+
+        var habit = { habit: exp.habit }
+        console.log('Patching Activity to Habit#update');
+        console.log(habit)
+
+        var req = {
+          method: 'PATCH',
+          url: 'http://localhost:3000/habits/' + exp.habit.id,
+          data: habit
+        }
+
+        $http(req)
+          .then(function(response){
+            console.log(response);
+            //add catch method.
+          });
+
+
+      }
     }
 
   });
