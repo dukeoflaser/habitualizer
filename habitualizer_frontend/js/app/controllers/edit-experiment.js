c95c562d78dfc8100694d70699ce5c783aa7e519
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-09 18:58:34 -0800

    Add submit function for edit experiment

--- habitualizer_frontend/js/app/controllers/edit-experiment.js
+++ habitualizer_frontend/js/app/controllers/edit-experiment.js
@@ -1,6 +1,6 @@
 angular
   .module('app')
-  .controller('EditExperimentController', function($scope, experiment) {
+  .controller('EditExperimentController', function($scope, $http, $state, experiment) {
 
     var exp = experiment.data.experiment;
 
@@ -10,5 +10,22 @@ angular
 
     console.log('The editing experiment:');
     console.log(experiment);
+
+    $scope.submit = function(exp){
+      var experiment = { experiment: exp };
+      var req = {
+        method: 'PATCH',
+        url: 'http://localhost:3000/experiments/' + exp.id,
+        data: experiment
+      }
+
+
+      $http(req)
+        .then(function(response){
+          console.log(response);
+          //add catch method. Break into named functions
+          $state.go('user.show.experiment', { id: response.data.experiment.id })
+        });
+    }
 
   });
