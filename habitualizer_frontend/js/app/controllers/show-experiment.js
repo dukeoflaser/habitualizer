3293e38825a487fa9242e59cdff03fd6ce69cc6d
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-11 01:13:14 -0800

    Tidy Up show experiment controller.

--- habitualizer_frontend/js/app/controllers/show-experiment.js
+++ habitualizer_frontend/js/app/controllers/show-experiment.js
@@ -1,13 +1,11 @@
 angular
   .module('app')
-  .controller('ShowExperimentController', function($scope, $state, $http, experimentData, activityData, experimentFactory, formProcessor) {
+  .controller('ShowExperimentController', function($scope, experimentData, activityData, experimentFactory, formProcessor) {
 
     $scope.experiment = experimentData.experiment
     if (activityData) $scope.activity = activityData.activity
 
-
     $scope.delete = function(submission) {
       formProcessor.processExpDelete(submission);
     }
-
   });
