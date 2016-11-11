477535d6f3c1e61994f0ffd04424721efd247a3a
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-10 21:34:57 -0800

    Extract submit function into it's own named function.

--- habitualizer_frontend/js/app/controllers/edit-habit.js
+++ habitualizer_frontend/js/app/controllers/edit-habit.js
@@ -1,6 +1,6 @@
 angular
   .module('app')
-  .controller('EditHabitController', function($scope, $state, $http, habitData, habitFactory) {
+  .controller('EditHabitController', function($scope, $state, habitData, habitFactory) {
 
     var hbt = habitData.habit;
 
@@ -8,30 +8,22 @@ angular
     $scope.habit.cue_attributes = hbt.cue;
     $scope.habit.reward_attributes = hbt.reward;
 
-    $scope.submit = function(hbt){
 
-      hbt.cue_attributes.has_been_updated = true;
-      hbt.reward_attributes.has_been_updated = true;
+    $scope.submit = function(submission){
+      processHabitUpdate(submission);
+    }
 
-      console.log('Patching to habits#update')
-      console.log(hbt);
+    function processHabitUpdate(h){
+      h.cue_attributes.has_been_updated = true;
+      h.reward_attributes.has_been_updated = true;
 
-      // var habit = { habit: hbt }
+      habitFactory.updateHabit(h.id, { habit: h })
 
-      habitFactory.updateHabit(hbt.id, { habit: hbt })
-
-      // var req = {
-      //   method: 'PATCH',
-      //   url: 'http://localhost:3000/habits/' + hbt.id,
-      //   data: habit
-      // }
-
-      // $http(req)
         .then(function(data){
-          console.log(data);
-          //add catch method.
           $state.go('user.show.habit', { id: data.habit.id });
         });
     }
+
+
 
   });
