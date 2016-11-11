b4ba03648216227f1182db6239b4bab854d14ef8
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-10 17:07:15 -0800

    Fix bugs that a) lock up the database by insuring only one submission is being made at a time and b) redirect to the next state before submission completes.

--- habitualizer_frontend/js/app/controllers/edit-experiment.js
+++ habitualizer_frontend/js/app/controllers/edit-experiment.js
@@ -5,39 +5,14 @@ angular
     var exp = experimentData.experiment;
     if (activityData) var activity = activityData.activity;
 
-
-
-
-    console.log('The Activity');
-    console.log(activity);
-
-    console.log('exp')
-    console.log(exp)
-
-
     $scope.experiment = exp;
     $scope.experiment.substitute_attributes = exp.substitute;
-    $scope.experiment.habit.activity_attributes = activity;
-    //double check to make sure habit/activity values are loaded.
+    if (activityData) $scope.experiment.habit.activity_attributes = activity;
 
-
-    console.log('The Initial editing experiment:');
-    console.log(experimentData);
-
-    // if(exp.habit.activity_id){
-    //   var activity_req = {
-    //     method: 'GET',
-    //     url: 'http://localhost:3000/activity/' + exp.habit.activity_id
-    //   }
-    //
-    //   $http(activity_request)
-    //     .then(function(response){
-    //       exp.habit.activity_attributes = response.data
-    //     });
-    // }
 
 
     $scope.submit = function(exp){
+      console.log('From submit experiment update - exp')
       console.log(exp);
       var experiment = { experiment: exp };
       var req = {
@@ -51,36 +26,55 @@ angular
         .then(function(response){
           console.log(response);
           //add catch method. Break into named functions
+
+
+
+                if(exp.habit.activity_attributes){
+                  /////////////////////// - Submit to Habit - Activity - ///////////////////////
+                  //Submit activity after experiment is finished submission to prevent database lockup.
+
+                  //check to make sure that exp is successful
+                  if(exp.successful == false){
+                    exp.habit.activity_attributes.description = "";
+                  }
+
+                  var habit = { habit: exp.habit }
+                  console.log('Patching Activity to Habit#update');
+                  console.log(habit)
+
+                  var req = {
+                    method: 'PATCH',
+                    url: 'http://localhost:3000/habits/' + exp.habit.id,
+                    data: habit
+                  }
+
+                  $http(req)
+                    .then(function(response){
+                      console.log(response);
+                      //add catch method.
+                    });
+
+                  
+                  $state.go('user.show.experiment', { id: response.data.experiment.id })
+
+                  /////////////////////////////////////////////////////////////////
+                } else {
+                  $state.go('user.show.experiment', { id: response.data.experiment.id })
+                }
+
+
+
+
+
+
+
+
+
+
           $state.go('user.show.experiment', { id: response.data.experiment.id })
         });
 
 
-
-      if(exp.habit.activity_attributes){
-        //check to make sure that exp is successful
-
-        if(exp.successful == false){
-          exp.habit.activity_attributes.description = "";
-        }
-
-        var habit = { habit: exp.habit }
-        console.log('Patching Activity to Habit#update');
-        console.log(habit)
-
-        var req = {
-          method: 'PATCH',
-          url: 'http://localhost:3000/habits/' + exp.habit.id,
-          data: habit
-        }
-
-        $http(req)
-          .then(function(response){
-            console.log(response);
-            //add catch method.
-          });
-
-
-      }
     }
 
   });
