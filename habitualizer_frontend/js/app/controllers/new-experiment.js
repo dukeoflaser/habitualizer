5207459a948cb30bd8e391af8981d4f525d8df29
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-11 00:30:03 -0800

    Tidy up new experiment view and controller.

--- habitualizer_frontend/js/app/controllers/new-experiment.js
+++ habitualizer_frontend/js/app/controllers/new-experiment.js
@@ -3,32 +3,30 @@ angular
   .controller('NewExperimentController', function($scope, rewardsData, habitData, experimentFactory, formProcessor) {
 
     var habits = habitData.habits;
-    $scope.habits = habits;
+    var lastHabit = habits[habits.length - 1];
 
+    $scope.habits = habits;
+    $scope.lastHabit = lastHabit;
     $scope.experiment = {};
 
-    var lastHabit = habits[habits.length - 1];
-    $scope.habit_name = lastHabit.name;
+    $scope.habitName = lastHabit.name;
     $scope.experiment.habit_id = lastHabit.id;
     $scope.currentReward = lastHabit.reward.craving;
+    $scope.rewards = rewardsData.rewards;
 
     $scope.selectHabit = function(habit){
       $scope.currentReward = habit.reward.craving;
       $scope.searchHabit.name = habit.name;
-      $scope.habit_name = habit.name;
+      $scope.habitName = habit.name;
       $scope.experiment.habit_id = habit.id;
     }
 
-    $scope.rewards = rewardsData.rewards;
     $scope.selectReward = function(reward){
-
       $scope.experiment.substitute_attributes = reward;
-      $scope.experiment.substitute_attributes.craving = reward.craving;
     }
 
     $scope.submit = function(submission){
       formProcessor.processExpCreate(submission);
     }
-
 
   });
