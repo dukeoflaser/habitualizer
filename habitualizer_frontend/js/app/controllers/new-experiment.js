d4116b6476065a5c4f73fc9be62de97edbba9c8d
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-11 08:56:59 -0800

    Wrap new experiment scoping in an if habits condition to prevent errors in the case of there not being any habits.

--- habitualizer_frontend/js/app/controllers/new-experiment.js
+++ habitualizer_frontend/js/app/controllers/new-experiment.js
@@ -3,30 +3,35 @@ angular
   .controller('NewExperimentController', function($scope, rewardsData, habitData, experimentFactory, formProcessor) {
 
     var habits = habitData.habits;
-    var lastHabit = habits[habits.length - 1];
+    if (habits) {
 
-    $scope.habits = habits;
-    $scope.lastHabit = lastHabit;
-    $scope.experiment = {};
+      var lastHabit = habits[habits.length - 1];
 
-    $scope.habitName = lastHabit.name;
-    $scope.experiment.habit_id = lastHabit.id;
-    $scope.currentReward = lastHabit.reward.craving;
-    $scope.rewards = rewardsData.rewards;
+      $scope.habits = habits;
+      $scope.lastHabit = lastHabit;
+      $scope.experiment = {};
 
-    $scope.selectHabit = function(habit){
-      $scope.currentReward = habit.reward.craving;
-      $scope.searchHabit.name = habit.name;
-      $scope.habitName = habit.name;
-      $scope.experiment.habit_id = habit.id;
-    }
+      $scope.habitName = lastHabit.name;
+      $scope.experiment.habit_id = lastHabit.id;
+      $scope.currentReward = lastHabit.reward.craving;
 
-    $scope.selectReward = function(reward){
-      $scope.experiment.substitute_attributes = reward;
-    }
+      $scope.rewards = rewardsData.rewards;
 
-    $scope.submit = function(submission){
-      formProcessor.processExpCreate(submission);
+      $scope.selectHabit = function(habit){
+        $scope.currentReward = habit.reward.craving;
+        $scope.searchHabit.name = habit.name;
+        $scope.habitName = habit.name;
+        $scope.experiment.habit_id = habit.id;
+      }
+
+      $scope.selectReward = function(reward){
+        $scope.experiment.substitute_attributes = reward;
+      }
+
+      $scope.submit = function(submission){
+        formProcessor.processExpCreate(submission);
+      }
+
     }
 
   });
