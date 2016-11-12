aa02a7396133f6bd27b46dea17f3bb5d98f5ec14
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-12 08:36:40 -0800

    Add appropriate validation messages.

--- habitualizer_frontend/js/angular-devise/controllers/registration.js
+++ habitualizer_frontend/js/angular-devise/controllers/registration.js
@@ -2,18 +2,16 @@ angular
   .module('app')
   .controller('RegistrationController', function(Auth, $scope, $state) {
     var vm = this;
-    vm.emailError = '';
-    vm.messageError = '';
-    
+
     vm.submit = function(user){
       var credentials = user;
 
       Auth.register(credentials).then(function(registeredUser) {
-          console.log(registeredUser); // => {id: 1, ect: '...'}
+
           $state.go('user.home');
       }, function(error) {
           // Registration failed...
-          console.log(error);
+          vm.hasEmailError = true;
           vm.emailError = error.config.data.user.email;
           vm.messageError = error.data.errors.email[0];
       });
