f54ea424e504934918f11922ca4e26e437012089
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-13 16:31:08 -0800

    Confgure app urls to not display the /#/

--- habitualizer_frontend/js/app/app.js
+++ habitualizer_frontend/js/app/app.js
@@ -1,5 +1,6 @@
 angular
   .module('app', ['Devise', 'ui.router', 'ngMessages', 'ngCookies'])
-  .config(function(AuthProvider){
+  .config(function(AuthProvider, $locationProvider){
     AuthProvider.baseUrl('http://localhost:3000');
+    $locationProvider.html5Mode(true);
   });
