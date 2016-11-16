88581b96ca371482fb9f24f9639c276846451264
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-15 22:04:07 -0800

    Fixed bug having to do with create/edit cues and rewards associated with a Habit.

--- habitualizer_api/app/models/habit.rb
+++ habitualizer_api/app/models/habit.rb
@@ -7,26 +7,21 @@ class Habit < ApplicationRecord
 
 
   def cue_attributes=(atts)
-    if atts['has_been_updated']
-      @cue = Cue.find(atts['id'])
-      @cue.update(name: atts['name'], nature: atts['nature'])
-    else
       @cue = Cue.find_or_create_by(name: atts['name'])
-      @cue.update(nature: atts['nature'])
-      self.cue = @cue
-    end
+
+      if @cue.nature == atts['nature']
+        self.cue = @cue
+      else
+        @cue = Cue.create(name: atts['name'], nature: atts['nature'])
+        self.cue = @cue
+      end
 
   end
 
   def reward_attributes=(atts)
-    if atts['has_been_updated']
-      @reward = Reward.find(atts['id'])
-      @reward.update(craving: atts['craving'])
-    else
       @reward = Reward.find_or_create_by(craving: atts['craving'])
       @reward.habits << self
       self.reward = @reward
-    end
   end
 
   def activity_attributes=(atts)
