82a17fe511fd0bfb34dcbeb8fab43e08be52aa36
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-15 23:27:18 -0800

    Style show experiment page and experimenet panel.

--- habitualizer_frontend/js/app/components/experiment-panel/experiment-panel.component.js
+++ habitualizer_frontend/js/app/components/experiment-panel/experiment-panel.component.js
@@ -1,7 +1,12 @@
 var ExperimentPanel = {
-
+  scope: {},
+  bindings: {
+    experiment: '<'
+  },
+  templateUrl: 'js/app/components/experiment-panel/experiment-panel.html',
+  controllerAs: 'user'
 }
 
 angular
   .module('app')
-  .component('hbExperiimentPanel', ExperimentPanel);
+  .component('hbExperimentPanel', ExperimentPanel);
