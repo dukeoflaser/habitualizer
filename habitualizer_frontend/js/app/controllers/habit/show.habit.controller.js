408949abf69288e1bf4b2851a6cba9a4a66611f4
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-25 15:28:25 -0800

    Style delete note 'X'. Remove note upon deletion. Hide Note Panel if there are know more notes to show.

--- habitualizer_frontend/js/app/controllers/habit/show.habit.controller.js
+++ habitualizer_frontend/js/app/controllers/habit/show.habit.controller.js
@@ -1,9 +1,11 @@
 function ShowHabitController($scope, habitData, experiments, currentExperiment, formProcessor, noteFactory) {
   var vm = this;
-  console.log(habitData);
+
   vm.habit = habitData.habit;
   vm.email = $scope.$parent.$parent.currentUser.email;
   vm.experiments = experiments;
+  vm.notePanel = false;
+  checkForNotes();
 
   if (currentExperiment) vm.experiment = currentExperiment.experiment;
 
@@ -26,14 +28,36 @@ function ShowHabitController($scope, habitData, experiments, currentExperiment, 
   }
 
   vm.deleteNote = function(note) {
-    noteFactory.deleteNote(note).then(function(){
+    noteFactory.deleteNote(note).then(function(response){
+      console.log('From THen ON...');
       note.displayed = false;
+      console.log(note);
+      vm.habit.notes.forEach(function(n, i, obj){
+        if (note.id == n.id) {
+          console.log('match');
+          obj.splice(i, 1);
+        }
+      });
+      checkForNotes();
     });
   }
 
   vm.habit.notes.forEach(function(note, i) {
     note.displayed = true;
   });
+
+  function checkForNotes() {
+    console.log(vm.habit.notes);
+    if (vm.habit.notes.length === 0) {
+      console.log('set to false');
+      vm.notePanel = false;
+
+      console.log(vm.notePanel);
+    } else {
+      vm.notePanel = true;
+    }
+  }
+
 }
 
 angular
